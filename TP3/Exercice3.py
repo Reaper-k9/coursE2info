82336d3d674c25d3637db190c2bc401042fc03f9
FatihhanCog <fatihhan.cog@reseau.eseo.fr>
2026-09-24 09:48:44 +0200

    exo3

--- TP3/Exercice3.py
+++ TP3/Exercice3.py
@@ -2,9 +2,9 @@ import sys
 
 print('argument list', sys.argv)
 ht = sys.argv[1]
-if ht.lower() != 'head' or ht.lower() != 'tail':
+if ht.lower() != 'head' and ht.lower() != 'tail':
     print('Error Value ! Entrer head ou tail')
-lignes = sys.argv[2]
+lignes = int(sys.argv[2])
 if lignes < 0:
     print("Error Value ! Entrer un nombre de lignes correct")
 loc = sys.argv[3]
@@ -13,16 +13,17 @@ try:
     if ht.lower() == 'head':
         with open(loc, 'r', encoding = 'utf-8') as f:
             for _ in range(lignes):
-                l = f.readlines()
+                l = f.readline()
                 if l == None:
                     break
-                print(l.strip())
+                print(l)
 
     elif ht.lower() == 'tail':
         with open(loc, 'r', encoding = 'utf-8') as f:
             l = f.readlines()
             for l in l[- lignes]:
-                if l == None:
+                if l == None:                 
                     break
+                print(l)
 except FileNotFoundError:
     print("Fcihier Introuvable")
