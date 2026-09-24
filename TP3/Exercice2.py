21ee12bcba5627177e8101907642de7f3dff5b22
FatihhanCog <fatihhan.cog@reseau.eseo.fr>
2026-09-24 08:43:22 +0200

    exo2

---
+++ TP3/Exercice2.py
@@ -0,0 +1,13 @@
+def Syraccus(a):
+    int(a)
+    print(a)
+    while a != 1:
+        r = a%2
+        if r == 0: 
+            a = a/2
+            print(a)
+        else: 
+            a = a*3 + 1
+            print(a)
+    return a
+print(Syraccus(1243))
